936bbb9ac0e01ac498033d033ae3f5f1d3146631
Xavier <xavierjimenezp@gmail.com>
2019-08-06 14:35:26 -0500

    Initial commit

---
+++ HIIGaussian.sh
@@ -0,0 +1,17 @@
+# 
+# This is hard coded
+#
+setenv MUSE_DATA_PATH "/nfs/ofelia/disk-b/sanchez/MUSE/AMUSING/analysis/dataproducts_SSP/";
+setenv MT_OUTPUT_PATH "/nfs/ofelia/disk-b/sanchez/MUSE/AMUSING/analysis/HIIGaussian/";
+setenv MI_OUTPUT_PATH "/nfs/ofelia/disk-b/sanchez/MUSE/AMUSING/analysis/HIIGaussian/";
+
+#
+# There is a look-back table with the minimum flux. Note
+#
+#table_flux_min.dat
+
+# Stored in  OUTPUT_TABLES_PATH
+
+#
+#
+#
